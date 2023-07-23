c060e60722c09638de0333f2f0c18c405f8495ac
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-07-23 11:02:44 -0600

    Created the logic of the databse, and configurated the webpack config to handle the css, the js and the manifest

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,3 +12,26 @@ const initdb = async () =>
         },
     });
 
+
+export const putDb = async (content) => {
+    console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({content: content});
+    const result = await request;
+    console.log('data saved to the database', result);
+};
+
+export const getDb = async () => {
+    console.log('GET from the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+}
+
+initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
@@ -18,12 +19,51 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE'
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: true, 
+        inject: true, 
+        name: 'JATE Text Editor', 
+        short_name: 'JATE', 
+        description: 'Text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i, 
+          use: [MiniCssExtractPlugin.loader, 'css-loader']
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            },
+          },
+        }
       ],
     },
   };
